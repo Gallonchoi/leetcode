30b26a8fbb50a9d1b49716bfe8a7044bc0013dac
GallonChoi <404842441@qq.com>
2014-10-22 19:05:39 +0800

    Add Linked list cycle

---
+++ linked-list-cycle/solution.py
@@ -0,0 +1,27 @@
+"""
+Linked List Cycle
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a boolean
+    def hasCycle(self, head):
+        while head != None:
+            if head.next == head:
+                return True
+            tmp = head.next
+            head.next = head
+            head = tmp
+        return False
